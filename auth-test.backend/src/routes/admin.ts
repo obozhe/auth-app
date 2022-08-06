db6c2f489eb54803afde74edea497e8943218678
Narek Avagyan <ava.narek@gmail.com>
2022-08-06 17:39:00 +0600

    updates

---
+++ auth-test.backend/src/routes/admin.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+import AdminController from '../controllers/admin-controller';
+import { isAdmin } from '../middlewares/user-role-check';
+
+const AdminRouter = express.Router();
+
+AdminRouter.route('/users').get(isAdmin, AdminController.getUsers);
+
+AdminRouter.route('/users/delete').patch(isAdmin, AdminController.deleteUsers);
+AdminRouter.route('/users/ban').patch(isAdmin, AdminController.banUsers);
+AdminRouter.route('/users/unban').patch(isAdmin, AdminController.unBanUsers);
+
+export default AdminRouter;
